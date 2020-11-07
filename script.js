299a90fe254307dfbc5bd38b1fecb87ce1c1c5e8
ismasgrove <45636192+ismasgrove@users.noreply.github.com>
2020-11-07 18:51:40 +0300

    basic layout

---
+++ script.js
@@ -0,0 +1,4 @@
+//import J from "jquery";
+//import THREE from "three";
+//import ANIME from "animejs";
+console.log("we have nothing at the moment");
---
+++ script.ts
@@ -0,0 +1,5 @@
+//import J from "jquery";
+//import THREE from "three";
+//import ANIME from "animejs";
+
+console.log("we have nothing at the moment");
