050ec103f0973d0b493e2cca5ad949bb9cef418a
ismasgrove <45636192+ismasgrove@users.noreply.github.com>
2020-11-10 22:54:28 +0300

    threejs beginnings

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-//import J from "jquery";
-//import THREE from "three";
-//import ANIME from "animejs";
-console.log("we have nothing at the moment");
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, 200 / 200, 0.1, 100);
+var surface = document.getElementById("sidebar_canvas");
+var renderer = new THREE.WebGLRenderer({
+  alpha: true,
+  antialias: true,
+  canvas: surface,
+});
+renderer.setSize(surface.width, surface.height);
+var geometry = new THREE.BoxGeometry();
+var material = new THREE.MeshBasicMaterial({ color: 0xda7386 });
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+camera.position.z = 5;
+var animate = function () {
+  requestAnimationFrame(animate);
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  renderer.render(scene, camera);
+};
+animate();
--- script.ts
+++
@@ -1,5 +0,0 @@
-//import J from "jquery";
-//import THREE from "three";
-//import ANIME from "animejs";
-
-console.log("we have nothing at the moment");
